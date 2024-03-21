8d319aa0f74e8eaf9c4bca4006a30e57ef910582
rapol-fantom <rapol@fantom.foundation>
2024-03-21 07:32:39 +0000

    actually push serve.go

--- backend/cmd/reporter/serve.go
+++ backend/cmd/reporter/serve.go
@@ -9,6 +9,8 @@ import (
 	"github.com/yargevad/filepathx"
 	"github.com/gorilla/mux"
 	"github.com/urfave/cli/v2"
+
+	amn "github.com/Fantom-foundation/LaScala/backend/utils"
 )
 
 var (
@@ -30,18 +32,47 @@ var (
 		Aliases: []string{"db"},
 		Value: "/home/rapolt/dev/db",
 	}
+
+	RedisAddressFlag = cli.StringFlag {
+		Name:   "redis-address",
+		Usage:	"address for redis",
+		Aliases: []string{"a", "ra"},
+		Value: 	"127.0.0.1",
+	}
+	RedisPortFlag = cli.IntFlag {
+		Name:   "redis-port",
+		Usage:	"port for redis",
+		Aliases: []string{"rp"},
+		Value: 	6379,
+	}
+	RedisTopicFlag = cli.StringFlag {
+		Name:   "redis-topic",
+		Usage:	"topic for redis",
+		Aliases: []string{"t", "rt"},
+		Value: 	"task-queue",
+	}
 )
 
 func ServeReporter(ctx *cli.Context) error {
+	worker := amn.NewWorker(
+		ctx.String("redis-address"),
+		ctx.Int("redis-port"),
+		ctx.String("redis-topic"),
+	)
+
 	return serveReporter(
 		ctx.String("static-dir"),
 		ctx.String("report-db"),
 		ctx.Int("port"),
+		worker,
 	)
 }
 
-func serveReporter(static string, reportDb string, port int) error {
+func serveReporter(static string, reportDb string, port int, worker *amn.Worker) error {
 	r := mux.NewRouter()
+
+	r.HandleFunc("/report/gen/{mid}/{rid}", generateReport(worker)).Methods("POST")
+	r.HandleFunc("/gen/{mid}/{rid}", generateReport(worker)).Methods("POST")
 
 	r.HandleFunc("/report/{mid}/{rid}", serveReport(static)).Methods("GET")
 	r.HandleFunc("/run/{rid}", serveReport(static)).Methods("GET")
@@ -101,7 +132,6 @@ func serveReport(static string) func(http.ResponseWriter, *http.Request) {
 			mid = filepath.Base(filepath.Dir(matches[0]))
 		}
 
-
 		pathToReport := filepath.Join(static, mid, fmt.Sprintf("%s.html", rid))
 		_, err := os.Stat(pathToReport)
 		if err != nil {
@@ -135,6 +165,30 @@ func serveReports(static string) func(http.ResponseWriter, *http.Request) {
 }
 
 func serveMasters(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/static/", http.StatusFound)
+	http.Redirect(w, r, "/static", http.StatusFound)
+}
+
+func generateReport(worker *amn.Worker) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+
+		mid, ok := vars["mid"]
+		if !ok {
+			serve400(w, r)
+			return
+		}
+
+		rid, ok := vars["rid"]
+		if !ok {
+			serve400(w, r)
+		}
+
+		fmt.Println("Here!", mid, rid)
+		worker.Push(r.Context(), &amn.Task{
+			Type: amn.TaskType("hello"),
+			MasterId: mid,
+			RunId: rid,
+		})
+	}
 }
 
